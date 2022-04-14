d27b44f09754a180a774e8f190fa82e9bc2f9a50
mynanne <hby8978@gmail.com>
2022-04-14 16:51:46 +0900

    06

--- 06/quiz5.js
+++ 06/quiz5.js
@@ -1,37 +1,34 @@
 const box1 = document.querySelector('.box');
 const box2 = document.querySelector('.box1');
 
-function boxMousedown(_event) {
-    this.mouseLeft = _event.clientX;
-    this.mouseTop = _event.clientY;
-    if(this.style.marginLeft && this.style.marginTop) {
-        this.mouseLeft -= parseInt(this.style.marginLeft, 10);
-        this.mouseTop -= parseInt(this.style.marginTop, 10);
+function makeBox(_box) {
+    this.mousedown = function(_event) {
+        this.mouseLeft = _event.clientX;
+        this.mouseTop = _event.clientY;
+        if(this.style.marginLeft && this.style.marginTop) {
+            this.mouseLeft -= parseInt(this.style.marginLeft, 10);
+            this.mouseTop -= parseInt(this.style.marginTop, 10);
+        }
+
+        this.boxClick = true;
     }
 
-    this.boxClick = true;
-}
-
-function boxMousemove(_box) {
-    return function(_event) { 
+    this.mousemove = function(_event) {
         if(_box.boxClick) {
             _box.style.marginLeft = _event.clientX -_box.mouseLeft +"px";
             _box.style.marginTop = _event.clientY -_box.mouseTop +"px";
         }
     }
-}
 
-function boxMouseup(_box) {
-    return function(_event) {
+    this.mouseup = function(_event) {
         _box.boxClick = false;
     }
+
+    
+    _box.addEventListener('mousedown',this.mousedown);
+    document.addEventListener('mousemove',this.mousemove);
+    document.addEventListener('mouseup',this.mouseup);
 }
 
-
-box1.addEventListener('mousedown',boxMousedown);
-document.addEventListener('mousemove',boxMousemove(box1));
-document.addEventListener('mouseup',boxMouseup(box1));
-
-box2.addEventListener('mousedown',boxMousedown);
-document.addEventListener('mousemove',boxMousemove(box2));
-document.addEventListener('mouseup',boxMouseup(box2));
+var makeBox1 = new makeBox(box1);
+var makeBox2 = new makeBox(box2);
